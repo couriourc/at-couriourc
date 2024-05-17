9b44bba9396d051afa7726e7f1f2a4b9f5f67fcc
chenrun <godakid@outlook.com>
2024-05-17 09:37:26 +0800

    docs preview

--- typedoc-pkg.config.mjs
+++ typedoc-pkg.config.mjs
@@ -4,22 +4,21 @@
 const config = [
 	// Basic example of API linking configured automatically with the local `package.json`.
 	{
-		linkPlugins   : ["es", "dom"],
-		output        : "docs",
-		monoRepo      : true,
-		path          : "packages",
+		linkPlugins: ["es", "dom"],
+		output: "docs",
+		monoRepo: true,
+		path: "packages",
 		typedocOptions: {
-			entryPointStrategy        : "packages",
-			externalPattern           : [
+			entryPointStrategy: "packages",
+			externalPattern: [
 				"**/node_modules/**",
 			],
-			plugin                    : [
-				"typedoc-plugin-merge-modules",
+			plugin: [
 				"typedoc-plugin-particles",
 			],
-			watch                     : true,
-			theme                     : "default",
-			particlesOptions          : {},
+			watch: true,
+			theme: "default",
+			particlesOptions: {},
 		},
 	},
 ]
